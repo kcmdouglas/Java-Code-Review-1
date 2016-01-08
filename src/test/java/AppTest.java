9b52c24a15a3e4f405b4500f2cb8840ad044f6d9
kcmdouglas <kcmdouglas@gmail.com>
2016-01-08 08:22:04 -0800

    Add code to pass second unit test.

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -4,17 +4,17 @@ import static org.junit.Assert.*;
 
 public class AppTest {
 
-  @Test
-  public void wordPuzzle_shouldIntakeStringOutputString_stringReturn() {
-    App app = new App();
-    String result = "I'm writing a string!";
-    assertEquals(result, app.wordPuzzle("I'm writing a string!"));
-  }
+  // @Test
+  // public void wordPuzzle_shouldIntakeStringOutputString_stringReturn() {
+  //   App app = new App();
+  //   String result = "I'm writing a string!";
+  //   assertEquals(result, app.wordPuzzle("I'm writing a string!"));
+  // }
 
   @Test
   public void wordPuzzle_shouldReplaceAWithDash_lDASHzy() {
     App app = new App();
     String result = "The quick brown fox jumps over the l-zy dog.";
-    assertEquals(result, app.wordPuzzle("The quick brown fox jomps over the lazt dog."));
+    assertEquals(result, app.wordPuzzle("The quick brown fox jumps over the lazy dog."));
   }
 }
